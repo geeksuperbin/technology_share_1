fda96f138aaeeb397e628158883d3d4b3bc6e7b4
liubin <hhdxlb@qq.com>
2020-09-18 17:29:27 +0800

    add

---
+++ test5.py
@@ -0,0 +1,10 @@
+
+fruit = {'apple','banana','pear','orange'}
+
+print(fruit)
+
+# 遍历打印set 集合中的内容
+for item in fruit:
+    print(item)
+
+
---
+++ test6.py
@@ -0,0 +1,10 @@
+
+geekbin = {'name':'刘彬','position':'技术负责','work':'5年'}
+
+
+for x,y in geekbin.items():
+    print(x,":",y)
+
+
+
+
